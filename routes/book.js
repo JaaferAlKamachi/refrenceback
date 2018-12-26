4c3e28b311a33566bdd0a5957c655b8a5720f9a2
Ramy Majid Naeem <rany.ramy2009@gmail.com>
2018-12-26 03:54:01 +0300

    added admin middlewares

--- routes/book.js
+++ routes/book.js
@@ -11,9 +11,7 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-const checker = require("../middlewares/checker");
 
-// router.use(checker); //change this
 router.use(express.json());
 
 /* ************************** MiddleWares ************************** */
@@ -21,46 +19,63 @@ router.use(express.json());
 // post middleWare to check for if the request is from an admin
 router.post("*", (req, res, next) => {
   console.log("post middleWare");
-  next();
+  checker(req, res, next);
 });
 
 // put middleWare to check for if the request is from an admin
 router.put("*", (req, res, next) => {
-  console.log("post middleWare");
-  next();
+  console.log("put middleWare");
+  checker(req, res, next);
 });
 
 // delete middleWare to check for if the request is from an admin
 router.delete("*", (req, res, next) => {
-  console.log("post middleWare");
-  next();
+  console.log("delete middleWare");
+  checker(req, res, next);
 });
 
 /* ************************** EOF MiddleWares ************************** */
+
+//list all books (change this to pagination later)
+router.get("/all", (req, res) => {
+  res.send("getting all books");
+});
+
+//get a specific book info
+router.get("/:id", (req, res) => {
+  res.send(`the book is ${req.params.id}`);
+});
 
 // //publish a book admin only
 router.post("/publish", (req, res) => {
   res.send("published");
 });
 
-/*
- *  Here all users should be able to view all and specific book info
- *
- *
- */
-
-//list all books (change this to pagination later) E
-router.get("/all", (req, res) => {
-  res.send("getting all books");
-});
-
-//get a specific book info E
-router.get("/:id", (req, res) => {
-  res.send(`the book is ${req.params.id}`);
-});
-
 //edit a publish Admin only
-//delete a publish Admin only
+router.put("/edit/:id", (req, res) => {
+  res.send("edited successfully");
+});
 
+//delete a publish Admin only
+router.delete("/delete/:id", (req, res) => {
+  res.send("deleted successfully");
+});
+
+// Important check paylod with db on every request
+function checker(req, res, next) {
+  const token = req.headers.token;
+  if (token) {
+    try {
+      //  Get the payload from the jsonwebtoken
+      let payload = jwt.verify(token, "key");
+      //  You can check the expiration if you want
+      next();
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  } else {
+    res.status(404).send("please log in");
+  }
+}
 //  Exporting the router so app.js can use it in a MiddleWare
 module.exports = router;
